eceacd60ccd5d34abb4d3207ec04ef10011da03e
devstefancho <skku7714@gmail.com>
2022-01-05 15:24:23 +0900

    add preset-env

--- dist/Hello.js
+++ dist/Hello.js
@@ -1,4 +1,14 @@
-import React from 'react';
-export default function Hello() {
-  return /*#__PURE__*/React.createElement("div", null, "Hello");
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = Hello;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function Hello() {
+  return /*#__PURE__*/_react["default"].createElement("div", null, "Hello");
 }
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
-import Hello from './Hello';
-export { Hello };
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Hello", {
+  enumerable: true,
+  get: function get() {
+    return _Hello["default"];
+  }
+});
+
+var _Hello = _interopRequireDefault(require("./Hello"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
